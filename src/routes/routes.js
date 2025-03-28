2e2f3a5a9a4c76682ee30c441b717da002eec317
Guilherme De Oliveira Sousa <guilherme.sousa184@etec.sp.gov.br>
2025-03-28 20:41:19 -0300

    routes

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const Avaliacao = require('../controllers/avaliacao');
+const Laboratorio = require('../controllers/laboratorio');
+
+router.get('/avaliacao', Avaliacao.ListarAvaliacao);
+router.get('/laboratorio', Laboratorio.ListarLaboratorio);
+
+module.exports = router;
