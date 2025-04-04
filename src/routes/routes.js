56d70c66ddfb23c79955e4c92bc39288906f37f4
Guilherme De Oliveira Sousa <guilherme.sousa184@etec.sp.gov.br>
2025-04-04 19:25:11 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,10 @@ const Avaliacao = require('../controllers/avaliacao');
 const Laboratorio = require('../controllers/laboratorio');
 
 router.get('/avaliacao', Avaliacao.ListarAvaliacao);
+
+
+
+
 router.get('/laboratorio', Laboratorio.ListarLaboratorio);
 
 module.exports = router;
